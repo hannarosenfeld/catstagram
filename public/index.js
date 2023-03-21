024705ff8f955c09fffcc67d983a8230176fe371
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2023-03-21 18:12:23 -0400

    init.

--- public/index.js
+++ public/index.js
@@ -1,1 +1,24 @@
 // Your code here
+// random img https://api.thecatapi.com/v1/images/search
+window.onload = async () => {
+    const app = document.getElementById("app");
+    const url = await fetch("https://api.thecatapi.com/v1/images/search");
+    const result = await url.json();
+    const data = result[0];
+
+    // set img height/ width
+
+    console.log("result: ",data)
+}
+
+
+// const getData = async () => {
+//     const app = document.getElementById("app");
+//     const url = await fetch("https://api.thecatapi.com/v1/images/search");
+//     const result = await url.json();
+//     const data = result[0];
+
+//     // set img height/ width
+
+//     console.log("result: ",data)
+// }
