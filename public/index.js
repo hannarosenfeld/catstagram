8bcfe7fbbfaaa049f8be63d644b5f6fb634e19f4
Prabhjot Singh <pjsingh1359@gmail.com>
2023-03-21 15:22:58 -0700

    finished day 1

--- public/index.js
+++ public/index.js
@@ -1,14 +1,28 @@
 // Your code here
 // random img https://api.thecatapi.com/v1/images/search
 window.onload = async () => {
-    const app = document.getElementById("app");
-    const url = await fetch("https://api.thecatapi.com/v1/images/search");
-    const result = await url.json();
-    const data = result[0];
+  const app = document.getElementById("app");
+  const url = await fetch("https://api.thecatapi.com/v1/images/search");
+  const result = await url.json();
+  const data = result[0];
 
-    // set img height/ width
+  const img = document.createElement('img');
+  const h1 = document.createElement('h1');
 
-    console.log("result: ",data)
+
+  h1.innerText = 'Kitten Pic';
+  img.setAttribute('src', data.url);
+
+  app.style.display = 'flex';
+  app.style.flexDirection = 'column';
+  app.style.alignItems = 'center';
+
+  app.appendChild(h1);
+  app.appendChild(img);
+  // set img height/ width
+
+  // console.log("result: ",data)
+
 }
 
 
